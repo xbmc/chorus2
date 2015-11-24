4ab817567baf81d71da758b7c45b3317d62ee37c
Jeremy Graham <mail@jez500.com>
2015-11-24 21:47:51 +1100

    Moved build copy to grunt, npm package version auto update, better code formatting in markdown

--- build.sh
+++ build.sh
@@ -5,17 +5,12 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
-echo "Update pages"
-echo "=============================================="
-PAGES_SRC=src/lang/en
-cp readme.md $PAGES_SRC/app-readme.md
-cp changelog.txt $PAGES_SRC/app-changelog.md
-cp src/lang/readme.md $PAGES_SRC/lang-readme.md
-
 echo "Update version number to $1"
 echo "=============================================="
 # This assumes the version number is on line 4 of dist/addon.xml
 sed -i "4s/version=\"[^\"]*/version=\"$1/" dist/addon.xml
+# And the first version match is for the package in package.json
+sed -i "s/version\": \"[^\"]*/version\": \"$1/" package.json
 
 echo "Running Grunt"
 echo "=============================================="
